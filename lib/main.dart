6beb19ffb84d81fd916b9e2c627539f215a935f8
Sebastian Ballen <sebastian.ballenc@gmail.com>
2021-11-09 23:05:35 -0500

    Diamong image widged edited.

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,14 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          title: const Text('I am Rich'),
+          title: const Center(
+            child: Text('I am Rich'),
+          ),
           backgroundColor: Colors.blueGrey[900],
         ),
         body: const Center(
           child: Image(
-            image: NetworkImage('https://media.npr.org/assets/img/2021/10/30/gettyimages-1206827173-54e4cc0070c4033b328742692236d0d4ca631d43-s1100-c50.jpg'),
+            image: AssetImage('images/diamond.png'),
           ),
         ),
       ),
